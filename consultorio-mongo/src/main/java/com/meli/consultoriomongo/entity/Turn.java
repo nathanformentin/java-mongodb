111761c000c1ae4ad880b57d8cbf31e23289a550
Nathan Formentin <nathan.formentin@mercadolivre.com>
2021-07-26 16:45:22 -0300

    adicionando tag de @Document na entidade

--- consultorio-mongo/src/main/java/com/meli/consultoriomongo/entity/Turn.java
+++ consultorio-mongo/src/main/java/com/meli/consultoriomongo/entity/Turn.java
@@ -3,17 +3,17 @@ package com.meli.consultoriomongo.entity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import java.time.LocalDate;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Document("exams")
 public class Turn {
 
     String id;
     Patient patient;
     Doctor doctor;
-    LocalDate date;
+    String date;
     String status;
 }
